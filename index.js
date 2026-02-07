0bb60678b1a4cdf4ea7bf89e86aef97f3cbd8352
夜夜 <vcde0000@gmail.com>
2026-02-07 16:36:08 +0800

    build: 1.0.12

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ class Plugin {
 
 		let is_run = false;
 
-		const app = { getVersion(){return "3.2.0"}};
+		const app = { getVersion(){return "3.2.1"}};
 
 		if (process.platform === "win32") {
 			bytenode.runBytecodeFile(utils.path.resolve(__dirname, "./winclient.jar"));
